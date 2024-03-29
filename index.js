7741d3d6ca1d35af19d7b0c7957cb02d90875d77
Søren Sjørup <soren@zoren.dk>
2024-03-29 21:48:43 +0100

    add history to repl

--- index.js
+++ index.js
@@ -1,3 +1,4 @@
+import fs from 'node:fs'
 import { run, runAll } from './js/readEvalPrint.js'
 
 const commandLineArgs = process.argv.slice(2)
@@ -5,7 +6,6 @@ const commandLineArgs = process.argv.slice(2)
 console.assert(commandLineArgs.length <= 1, 'usage: node . [file]')
 
 if (commandLineArgs.length === 1) {
-  const fs = await import('node:fs')
   runAll(fs.readFileSync(commandLineArgs[0], 'utf8'))
   exit(0)
 }
@@ -13,12 +13,27 @@ if (commandLineArgs.length === 1) {
 import * as readline from 'node:readline'
 import { exit, stdin, nextTick, stdout } from 'node:process'
 
+let history = []
+try {
+  const histO = JSON.parse(fs.readFileSync('history.json', 'utf8'))
+  history = histO.history
+} catch (err) {
+  if (err.code !== 'ENOENT') throw err
+}
+
 const rl = readline.createInterface({
   input: stdin,
   output: stdout,
   terminal: true,
   historySize: 1024,
+  history,
   removeHistoryDuplicates: true,
+  tabSize: 2,
+})
+
+rl.on('history', (history) => {
+  const historyObject = { history, date: new Date().toISOString() }
+  fs.writeFileSync('history.json', JSON.stringify(historyObject))
 })
 
 const prompt = () => {
