2a29f14dd45aa9f1279ea9090b2532f5ca516bde
Søren Sjørup <soren@zoren.dk>
2024-03-30 21:31:52 +0100

    make diff asserts for run and comptime

--- js/compile.js
+++ js/compile.js
@@ -1,12 +1,16 @@
-const assert = (cond, msg) => {
+const ctAssert = (cond, msg) => {
   if (!cond) throw new Error('COMPILE: ' + msg)
+}
+
+const rtAssert = (cond, msg) => {
+  if (!cond) throw new Error('RUNTIME: ' + msg)
 }
 
 import { isSymbol } from './lib.js'
 
 const assertSymbol = (form, msg) => {
   const name = isSymbol(form)
-  assert(name, msg)
+  ctAssert(name, msg)
   return name
 }
 
@@ -32,24 +36,24 @@ export const compile = (ast) => {
           console.log('undefined symbol: ' + symbolName)
           console.log(env)
         }
-        assert(env.has(symbolName), 'undefined symbol: ' + symbolName)
+        rtAssert(env.has(symbolName), 'undefined symbol: ' + symbolName)
         return env.get(symbolName)
       }
     }
   }
-  assert(Array.isArray(ast), 'ast must be an array at this point')
+  ctAssert(Array.isArray(ast), 'ast must be an array at this point')
   if (ast.length === 0) return () => ast
   const [first, ...rest] = ast
   const firstName = assertSymbol(first, 'first element must be a symbol')
   switch (firstName) {
     case 'if': {
-      assert(rest.length === 3, 'if must have 3 arguments')
+      ctAssert(rest.length === 3, 'if must have 3 arguments')
       const ccond = compile(rest[0])
       const cthen = compile(rest[1])
       const celse = compile(rest[2])
       return (env, fenv) => {
         const econd = ccond(env, fenv)
-        assert(
+        rtAssert(
           typeof econd === 'number' && !isNaN(econd),
           'condition must be a number',
         )
@@ -57,10 +61,9 @@ export const compile = (ast) => {
       }
     }
     case 'func': {
-      assert(rest.length >= 3, 'func must have at least 3 arguments')
-
+      ctAssert(rest.length >= 3, 'func must have at least 3 arguments')
       const fname = assertSymbol(rest[0], 'first argument must be a symbol')
-      assert(Array.isArray(rest[1]), 'second argument must be a list')
+      ctAssert(Array.isArray(rest[1]), 'second argument must be a list')
       const paramNames = rest[1].map((x) =>
         assertSymbol(x, 'parameters must be symbols'),
       )
@@ -70,7 +73,7 @@ export const compile = (ast) => {
 
       return (env, fenv) => {
         fenv.set(fname, (...args) => {
-          assert(
+          rtAssert(
             args.length === arity,
             'wrong number of arguments to function: ' + fname,
           )
@@ -84,10 +87,10 @@ export const compile = (ast) => {
     }
     case 'let':
     case 'loop': {
-      assert(rest.length >= 2, firstName + ' must have at least 2 arguments')
+      ctAssert(rest.length >= 2, firstName + ' must have at least 2 arguments')
       const [bindings, ...bodies] = rest
-      assert(Array.isArray(bindings), 'second argument must be a list')
-      assert(bindings.length % 2 === 0, 'bindings must be of even length')
+      ctAssert(Array.isArray(bindings), 'second argument must be a list')
+      ctAssert(bindings.length % 2 === 0, 'bindings must be of even length')
       const cbindings = [...pairwise(bindings)].map(([name, form]) => [
         assertSymbol(name, 'key must be a symbol'),
         compile(form),
@@ -117,7 +120,7 @@ export const compile = (ast) => {
           const result = lastBody(newEnv, fenv)
           if (!(result instanceof Recur)) return result
           const { args } = result
-          assert(
+          rtAssert(
             bindingCount === args.length,
             'wrong number of arguments to cont',
           )
@@ -131,28 +134,28 @@ export const compile = (ast) => {
       return (env, fenv) => new Recur(cargs.map((c) => c(env, fenv)))
     }
     case 'case': {
-      assert(rest.length >= 2, 'switch must have at least 2 arguments')
-      assert(
+      ctAssert(rest.length >= 2, 'case must have at least 2 arguments')
+      ctAssert(
         rest.length % 2 === 0,
-        'switch must have an even number of arguments',
+        'case must have an even number of arguments',
       )
       const [key, ...cases] = rest
       const ckey = compile(key)
       const cCases = []
       const usedKeys = new Set()
       const checkDuplicateKeys = (k) => {
-        assert(!usedKeys.has(k), 'duplicate case key: ' + k)
+        ctAssert(!usedKeys.has(k), 'duplicate case key: ' + k)
         usedKeys.add(k)
       }
       for (const [caseKey, caseBody] of pairwise(cases)) {
         const cKeys = []
         if (typeof caseKey === 'number') {
-          assert(!isNaN(caseKey), 'case key must be a number')
+          ctAssert(!isNaN(caseKey), 'case key must be a number')
           checkDuplicateKeys(caseKey)
           cKeys.push(caseKey)
         } else if (Array.isArray(caseKey)) {
           for (const k of caseKey) {
-            assert(
+            ctAssert(
               typeof k === 'number',
               'case key in list must be a number ' + k,
             )
@@ -160,14 +163,14 @@ export const compile = (ast) => {
             cKeys.push(k)
           }
         } else {
-          assert(false, 'illegal case key')
+          ctAssert(false, 'illegal case key')
         }
         cCases.push([cKeys, compile(caseBody)])
       }
       const cdefaultCase = compile(cases.at(-1))
       return (env) => {
         const ekey = ckey(env)
-        assert(typeof ekey === 'number', 'switch key must be a number')
+        rtAssert(typeof ekey === 'number', 'case key must be a number')
         for (const [cKeys, cBody] of cCases) {
           for (const k of cKeys) {
             if (k === ekey) return cBody(env)
@@ -181,7 +184,7 @@ export const compile = (ast) => {
   const cargs = rest.map(compile)
   return (env, funcEnv) => {
     const fn = funcEnv.get(firstName)
-    assert(fn, 'undefined function: ' + firstName)
+    rtAssert(fn, 'undefined function: ' + firstName)
     return fn(...cargs.map((c) => c(env, funcEnv)))
   }
 }
