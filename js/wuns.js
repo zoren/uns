4aebb55aa65827ba0956cf726b2d5b2d5383af21
Søren Sjørup <soren@zoren.dk>
2024-04-10 00:01:59 +0200

    little eval

--- js/wuns.js
+++ js/wuns.js
@@ -59,7 +59,7 @@ for (const [expected, input] of lexTests) {
   )
 }
 
-export const parse = (inputString) => {
+export const makeParser = (inputString) => {
   const lexer = makeLexer(inputString)
   let token = null
   const next = () => {
@@ -69,9 +69,9 @@ export const parse = (inputString) => {
   next()
   const currentToken = () => token
 
-  const readForm = () => {
+  const go = () => {
     const token = currentToken()
-    assert(token !== null, 'unexpected end of input in readForm')
+    if (token === null) return null
     next()
     switch (token) {
       case '[': {
@@ -83,7 +83,7 @@ export const parse = (inputString) => {
             next()
             break
           }
-          list.push(readForm())
+          list.push(go())
         }
         return Object.freeze(list)
       }
@@ -94,7 +94,7 @@ export const parse = (inputString) => {
     }
   }
 
-  return readForm
+  return go
 }
 
 const parseTests = [
@@ -112,11 +112,79 @@ const parseTests = [
 ]
 
 for (const [expected, input] of parseTests) {
-  const actual = parse(input)()
+  const actual = makeParser(input)()
   const jsonExpected = JSON.stringify(expected)
   const jsonActual = JSON.stringify(actual)
   assert(
     jsonExpected === jsonActual,
     `for '${input}' expected ${jsonExpected} but got ${jsonActual}`,
   )
+}
+
+const print = (x) => {
+  if (typeof x === 'string') return x
+  if (Array.isArray(x)) return `[${x.map(print).join(' ')}]`
+  throw new Error(`cannot print ${x}`)
+}
+
+const printTests = [
+  ['a', 'a'],
+  ['a', ' a '],
+  ['a', ' a'],
+  ['[]', '[]'],
+  ['[]', ' [ ] '],
+  ['[a]', '[a]'],
+  ['[a]', ' [ a ] '],
+  ['[a b c]', ' [ a b c ] '],
+  ['[a [b c]]', ' [ a [ b c ] ] '],
+  ['[a [b c]]', ' [ a [ b c ] ] '],
+  ['[a [b c d]]', ' [ a [ b c d ] ] '],
+]
+
+for (const [expected, input] of printTests) {
+  const actual = print(makeParser(input)())
+  assert(
+    expected === actual,
+    `for '${input}' expected ${expected} but got ${actual}`,
+  )
+}
+
+const wunsEval = (form) => {
+  if (typeof form === 'string') return env.get(form)
+
+  assert(Array.isArray(form), `cannot eval ${form} expected string or array`)
+
+  const [firstWord, ...args] = form
+  switch (firstWord) {
+    case 'if': {
+      const econd = wunsEval(args[0])
+      if (econd === '0') return wunsEval(args[2])
+      return wunsEval(args[1])
+    }
+    case 'quote':
+      return args[0]
+  }
+  // const func = env.get(firstWord)
+  // return func(...args.map((arg) => eval(env, arg)))
+}
+
+const tests = `
+[if [quote 0] [quote t] [quote f]] [.= f]
+[if [quote 1] [quote t] [quote f]] [.= t]
+`
+
+const parse = makeParser(tests)
+
+let prev = null
+
+while (true) {
+  const form = parse()
+  if (form === null) break
+  if (!Array.isArray(form)) continue
+  const [first, second] = form
+  if (first !== '.=') {
+    prev = wunsEval(form)
+    continue
+  }
+  if (prev !== null) assert(print(prev) === print(second), 'expected equal')
 }
